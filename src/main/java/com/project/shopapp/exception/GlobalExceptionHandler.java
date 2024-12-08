47b1388d207d7a9c0fd7cb0beceed4c0f3fec4ee
Minh Dinh <minhdv.dev@gmail.com>
2024-12-08 17:38:52 +0700

    feat: Add custom exceptions and enhance global exception handling

--- src/main/java/com/project/shopapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/shopapp/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.project.shopapp.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,15 +16,37 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = RuntimeException.class)
     public ResponseEntity<ApiResponse<?>> handleRuntimeException(RuntimeException e) {
         log.error("Exception caught", e);
-        ApiResponse<?> response = new ApiResponse<>(500, e.getMessage(), null);
-        return ResponseEntity.status(500).body(response);
+        ApiResponse<?> response = new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage(), null);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         log.error("Exception caught", e);
-        ApiResponse<?> response =
-                new ApiResponse<>(400, e.getBindingResult().getFieldError().getDefaultMessage(), null);
-        return ResponseEntity.status(400).body(response);
+        ApiResponse<?> response = new ApiResponse<>(
+                HttpStatus.BAD_REQUEST.value(),
+                e.getBindingResult().getFieldError().getDefaultMessage(),
+                null);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(value = DataNotFoundException.class)
+    public ResponseEntity<ApiResponse<?>> handleDataNotFoundException(DataNotFoundException e) {
+        log.error("Exception caught", e);
+        ApiResponse<?> response = ApiResponse.<String>builder()
+                .code(HttpStatus.NOT_FOUND.value())
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(value = InvalidPasswordException.class)
+    public ResponseEntity<ApiResponse<?>> handleInvalidPasswordException(InvalidPasswordException e) {
+        log.error("Exception caught", e);
+        ApiResponse<?> response = ApiResponse.<String>builder()
+                .code(HttpStatus.UNAUTHORIZED.value())
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
 }
