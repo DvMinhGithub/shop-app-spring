581cb762322ba0dd08b8b5b44500eca18b2eb49f
Minh Dinh <minhdv.dev@gmail.com>
2024-12-08 17:22:30 +0700

    refactor: Refactor api response

--- src/main/java/com/project/shopapp/dto/response/ApiResponse.java
+++ src/main/java/com/project/shopapp/dto/response/ApiResponse.java
@@ -3,17 +3,19 @@ package com.project.shopapp.dto.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 public class ApiResponse<T> {
-    int status;
+    int code;
     String message;
     T result;
 }
