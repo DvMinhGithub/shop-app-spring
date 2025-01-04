4c780be4ecea8eb3922a644f74c34e924419d9aa
Minh Dinh <minhdv.dev@gmail.com>
2025-01-04 17:30:32 +0700

    chore: Update database schema and disable Flyway migrations

--- src/main/resources/db/migration/V1.0__schema.sql
+++ src/main/resources/db/migration/V1.0__schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE roles (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE users(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
     fullname VARCHAR(100) DEFAULT '',
     phone_number VARCHAR(10) NOT NULL,
     address VARCHAR(200) DEFAULT '',
@@ -13,54 +13,53 @@ CREATE TABLE users(
     updated_at DATETIME,
     is_active TINYINT(1) DEFAULT 1,
     date_of_birth DATE,
-    facebook_account_id INT DEFAULT 0,
-    role_id INT
+    facebook_account_id BIGINT DEFAULT 0,
+    role_id BIGINT
 );
 
 CREATE TABLE tokens (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
     token VARCHAR(255) UNIQUE NOT NULL,
     token_type VARCHAR(50) NOT NULL,
     expiration_date DATETIME,
     revoked TINYINT(1) NOT NULL,
     expired TINYINT(1) NOT NULL,
-    user_id INT
+    user_id BIGINT
 );
 
 CREATE TABLE social_accounts(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
     provider VARCHAR(20) NOT NULL COMMENT 'Tên nhà social network',
     provider_id VARCHAR(50) NOT NULL,
     email VARCHAR(150) NOT NULL COMMENT 'Email tài khoản',
     name VARCHAR(100) NOT NULL COMMENT 'Tên người dùng',
-    user_id INT
+    user_id BIGINT
 );
 
 CREATE TABLE categories(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL DEFAULT '' COMMENT 'Tên danh mục, vd: đồ điện tử'
 );
 
 CREATE TABLE products(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(350) COMMENT 'Tên sản phẩm',
     price FLOAT NOT NULL CHECK(price >= 0),
-    thumbnail VARCHAR(300) DEFAULT '',
     description LONGTEXT,
     created_at DATETIME,
     updated_at DATETIME,
-    category_id INT
+    category_id BIGINT
 );
 
 CREATE TABLE product_images(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    product_id INT,
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    product_id BIGINT,
     image_url VARCHAR(300) NOT NULL
 );
 
 CREATE TABLE orders (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT,
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    user_id BIGINT,
     fullname VARCHAR(100) DEFAULT '',
     email VARCHAR(100) DEFAULT '',
     phone_number VARCHAR(20) NOT NULL,
@@ -78,9 +77,9 @@ CREATE TABLE orders (
 );
 
 CREATE TABLE order_details(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT,
-    product_id INT,
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    order_id BIGINT,
+    product_id BIGINT,
     price FLOAT CHECK(price >= 0),
     number_of_products INT CHECK(number_of_products > 0),
     total_money FLOAT CHECK(total_money >= 0),
